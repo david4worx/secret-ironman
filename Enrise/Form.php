67e16aff52c4ddad8a977e429e909142018cf593
David Papadogiannakis <david@enrise.com>
2012-11-16 21:03:13 +0100

    removed crappy files

--- Enrise/Form.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Enrise_Form extends Zend_Form
-{
-    public static function manager($name)
-    {
-        return new self(Glitch_Registry::getConfig()->get('forms')->{$name});
-    }
-}
